f944994e0ae11720091b1a1835bb32409e42fce2
siddharthbankar12 <siddharthbankar1204@gmail.com>
2025-04-01 16:09:18 +0530

    assignment 29 march

--- Backend/index.js
+++ Backend/index.js
@@ -2,17 +2,40 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("welcome");
+  return res.send("welcome to backend server");
 });
-app.get("/books", (req, res) => {
-  res.send("my books.");
-});
-app.get("/products", (req, res) => {
-  res.send("my products.");
-});
+
 app.get("/hello", (req, res) => {
-  res.send("kasay madali");
+  return res.send("kasay madali");
+});
+
+app.get("/books", (req, res) => {
+  return res.send("my books");
+});
+
+app.get("/products", (req, res) => {
+  return res.send("my products");
+});
+
+app.post("/register", (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body;
+    console.log(name, email, password, confirmPassword);
+
+    if (!name || !email || !password || !confirmPassword) {
+      return res.send("please check if anything is missing");
+    }
+    if (password !== confirmPassword) {
+      return res.send("password not match");
+    }
+
+    return res.send("register successfully");
+  } catch (error) {
+    return res.send(error);
+  }
 });
 
 app.listen(8000, () => console.log("server is running on port localhost:8000"));
